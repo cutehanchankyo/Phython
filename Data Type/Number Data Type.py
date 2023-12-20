50bf487b91bd26cac778f93ab3456a7b378ae081
jeongchangyo <s22070@gsm.hs.kr>
2023-12-20 23:28:29 +0900

    Nmber Data Type

---
+++ Data Type/Number Data Type.py
@@ -0,0 +1,15 @@
+print(-1)   #-1
+print(0)    #0
+print(1)    #1
+print(1.1)  #float
+print(1+1)  #2
+print('1+1',1+1)    #1+1 2
+print('2*4',2*4)    #2*4 8
+print('8/2',8/2)    #8/2 4
+
+import math
+print('math.sqrt(4)',math.sqrt(4))  #sqrt(a) 함수는 a의 제곱근을 구하는 함수
+print('math.pow(4,2)',math.pow(4,2))    #pow(a,b) 함수는 a를 b만큼 제곱한 값을 구하는 함수
+
+import random
+print('random.random()',random.random())    #random() 랜덤한 값을 구하는 함수
